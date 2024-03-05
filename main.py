ff8c3b977ad69186b34873450cb1da22b701a7e5
hieundx <hieundx@proton.me>
2024-03-05 17:03:41 +0700

    Use inference API instead of loading model into local

--- main.py
+++ main.py
@@ -1,14 +1,32 @@
-from transformers import pipeline
+# from transformers import pipeline
 import streamlit as st
+import requests
+import os
+import dotenv
 
-pipeline_en = pipeline('translation', 'Helsinki-NLP/opus-mt-en-vi')
-pipeline_vi = pipeline('translation', 'Helsinki-NLP/opus-mt-vi-en')
+dotenv.load_dotenv()
 
+# Local inference
+# pipeline_en = pipeline('translation', 'Helsinki-NLP/opus-mt-en-vi')
+# pipeline_vi = pipeline('translation', 'Helsinki-NLP/opus-mt-vi-en')
 
+# Use cloud-based inference API due to Streamlit sharing's limitation
+headers = {"Authorization": f"Bearer {os.environ['HF_API_KEY']}"}
+
+def query(api_url, text):
+	response = requests.post(api_url, headers=headers, json={ 'inputs': text })
+	return response.json()
+
+def pipeline_en(text):
+     return query("https://api-inference.huggingface.co/models/Helsinki-NLP/opus-mt-en-vi", text)
+
+def pipeline_vi(text):
+     return query("https://api-inference.huggingface.co/models/Helsinki-NLP/opus-mt-vi-en", text)
+	
 st.title('English -> Tiếng Việt')
 
-def generate_response(input_text, pipeline):
-    response = pipeline(input_text)
+def generate_response(text, pipeline):
+    response = pipeline(text)
 
     st.info(response[0]['translation_text'])
 
